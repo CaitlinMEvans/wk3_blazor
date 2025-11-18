ee682226e0415b2c36068cee934eca500967e159
Caitlin Evans <caitmsevans@gmail.com>
2025-11-17 18:37:56 -0700

    week 3 blazing pizzas

---
+++ BlazingPizza2/Models/Pizza.cs
@@ -0,0 +1,11 @@
+namespace BlazingPizza.Models;
+
+public class Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public bool Vegetarian { get; set; }
+    public bool Vegan { get; set; }
+}
